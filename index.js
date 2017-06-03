116cddb31b37e7fb2e1e8e5d6a07d593847b14ec
Juan <fernandez@fazua.com>
2017-06-03 23:09:03 +0200

    Scraping and uploading to firebase

--- index.js
+++ index.js
@@ -1,13 +1,104 @@
-var firebase = require('firebase-admin')
+var admin = require('firebase-admin')
+var Xray = require('x-ray')
 
+var _ = require('lodash')
+var fs = require('fs')
 
-var serviceAccount = require('credentials/')
+var xray = new Xray();
+
+var serviceAccount = require('./credentials/mymanager-d47ac-firebase-adminsdk-nij3l-141a31a2f0.json')
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://"
+	databaseURL: "https://mymanager-d47ac.firebaseio.com"
 })
 
-admin.database().ref('').on('value', (snapshot)=>{
+
+
+	xray('https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1',
+		'div#yw1 > table > tbody > tr > td.hauptlink.hide-for-small > a.vereinprofil_tooltip',
+		[{
+			href:'@href',
+		}])((err,teams)=>{
+			if(err){
+				console.error('ERROR', err)
+			}else{
+				teams.forEach((team)=>{
+					xray(team.href,['a.spielprofil_tooltip'])((err,players)=>{
+						if(err){
+							console.error("ERROR",err)
+						}else{
+							players.forEach((player)=>{
+								console.log(player)
+								admin.database().ref('players').set(
+								{
+									name: player,
+									team: team.href
+								})
+							})
+						}
+						//players.split(',').forEach((player)=>{
+							//console.log(player)
+						/*	admin.database().ref('players').set(
+							{
+								name: player,
+								team: team.href
+							})*/
+						//})
+					})
+				})
+			}
+
+		})
+
+			/*
+			teams.forEach((team)=>{
+			    xray(team.href, 'a.spielprofil_tooltip'
+			)((err,players)=>{
+					console.log(players)
+					if(err){
+						console.error("ERROR",err)
+					}else{
+						players.forEach((player)=>{
+							console.log(`player: ${player}`)
+						})
+
+					}
+
+				})
+			})
+
+		})*/
+
+var player_database;
+
+admin.database().ref('players').on('value', (snapshot)=>{
+	player_database = snapshot.val();
 	console.log(snapshot.val())
 })
+/*
+var serviceAccount = require('./credentials/mymanager-d47ac-firebase-adminsdk-nij3l-141a31a2f0.json')
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://mymanager-d47ac.firebaseio.com"
+})
+
+admin.database().ref('messages').on('value', (snapshot)=>{
+	player_database = snapshot.val();
+	console.log(snapshot.val())
+})
+*/
+/*
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log(`app listening to requests in port ${process.env.PORT}`);
+})
+
+app.get('/', (req,res)=>{
+	res.send('Welcome to players API')
+})
+
+app.get('/players', (req,res)=>{
+	res.json(player_database)
+})
+*/
