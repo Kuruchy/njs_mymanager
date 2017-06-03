c45fc125540de8f826c6bca4918e464e6711de6b
Juan <fernandez@fazua.com>
2017-06-03 23:27:31 +0200

    cleaning code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var admin = require('firebase-admin')
 var Xray = require('x-ray')
-
-var _ = require('lodash')
-var fs = require('fs')
-
 var xray = new Xray();
 
 var serviceAccount = require('./credentials/mymanager-d47ac-firebase-adminsdk-nij3l-141a31a2f0.json')
@@ -14,91 +10,35 @@ admin.initializeApp({
 })
 
 
-
-	xray('https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1',
-		'div#yw1 > table > tbody > tr > td.hauptlink.hide-for-small > a.vereinprofil_tooltip',
-		[{
-			href:'@href',
-		}])((err,teams)=>{
-			if(err){
-				console.error('ERROR', err)
-			}else{
-				teams.forEach((team)=>{
-					xray(team.href,['a.spielprofil_tooltip'])((err,players)=>{
-						if(err){
-							console.error("ERROR",err)
-						}else{
-							players.forEach((player)=>{
-								console.log(player)
-								admin.database().ref('players').push(
-								{
-									name: player,
-									team: team.href
-								})
-							})
-						}
-						//players.split(',').forEach((player)=>{
-							//console.log(player)
-						/*	admin.database().ref('players').set(
-							{
-								name: player,
-								team: team.href
-							})*/
-						//})
-					})
-				})
-			}
-
-		})
-
-			/*
+// scraping transfermarkt
+xray('https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1',
+	'div#yw1 > table > tbody > tr > td.hauptlink.hide-for-small > a.vereinprofil_tooltip',
+	[{ href:'@href'}])
+	((err,teams)=>{
+		if(err){
+			console.error('ERROR', err)
+		}else{
 			teams.forEach((team)=>{
-			    xray(team.href, 'a.spielprofil_tooltip'
-			)((err,players)=>{
-					console.log(players)
+				xray(team.href,['a.spielprofil_tooltip'])
+				((err,players)=>{
 					if(err){
 						console.error("ERROR",err)
 					}else{
 						players.forEach((player)=>{
-							console.log(`player: ${player}`)
+							admin.database().ref('players').push(
+								{
+									name: player,
+									team: team.href
+								}
+							)
 						})
-
 					}
-
 				})
 			})
+		}
+	})
 
-		})*/
-
-var player_database;
-
+// to keep track of changes in the database
 admin.database().ref('players').on('value', (snapshot)=>{
 	player_database = snapshot.val();
-	console.log(snapshot.val())
 })
-/*
-var serviceAccount = require('./credentials/mymanager-d47ac-firebase-adminsdk-nij3l-141a31a2f0.json')
-
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://mymanager-d47ac.firebaseio.com"
-})
-
-admin.database().ref('messages').on('value', (snapshot)=>{
-	player_database = snapshot.val();
-	console.log(snapshot.val())
-})
-*/
-/*
-app.listen(process.env.PORT || 3000, ()=>{
-	console.log(`app listening to requests in port ${process.env.PORT}`);
-})
-
-app.get('/', (req,res)=>{
-	res.send('Welcome to players API')
-})
-
-app.get('/players', (req,res)=>{
-	res.json(player_database)
-})
-*/
