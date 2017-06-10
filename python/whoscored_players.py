1ad3d6d27d9c4081f6c0853343eb7fb3bbb9d78c
Juan <fernandez@fazua.com>
2017-06-10 15:43:36 +0200

    Now also able to get a screenshot of the webpage

--- python/whoscored_players.py
+++ python/whoscored_players.py
@@ -3,6 +3,8 @@ import incapsula
 import pdb
 import scrapy
 
+import base64
+
 from scrapy.crawler import CrawlerProcess
 
 import lxml.html
@@ -39,11 +41,22 @@ class MySpider(scrapy.Spider):
 	}
 
 	def start_requests(self):
+		splash_args = {
+			'html': 1,
+			'png': 1,
+			'width': 600,
+			'render_all': 1,
+			'wait': 0.5
+		}
 		for url in self.start_urls:
-			yield SplashRequest(url, self.parse, args={'wait':0.5})
+			yield SplashRequest(url, self.parse, endpoint='render.json', args=splash_args)
 
 
 	def parse(self,response):
+		png_bytes = base64.b64decode(response.data['png'])
+		image = open('screenshot.png','wb')
+		image.write(png_bytes)
+		image.close()
 		results = open('results.json','w')
 		for team in response.css('tbody.standings > tr > td > a.team-link'):
 			name = team.css('::text').extract()[0]
